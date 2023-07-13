9ba66deba78e619b301e08fb88bb4dfa5c94b6e1
Precious Nnam <nnamvannessa@gmail.com>
2023-07-13 22:06:51 +0100

    signup service function completed

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class AuthService {
@@ -11,16 +15,30 @@ export class AuthService {
     const hash = await argon.hash(dto.password);
 
     //save the user in the database
-    const user = await this.prisma.user.create({
-      data: {
-        username: dto.username,
-        email: dto.email,
-        hash,
-        type: dto.type,
-      },
-    });
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          username: dto.username,
+          email: dto.email,
+          hash,
+          type: dto.type,
+        },
+      });
 
-    return user;
+      return user;
+    } catch (error) {
+      if (
+        error instanceof
+        Prisma.PrismaClientKnownRequestError
+      ) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException(
+            'Credentials already exist',
+          );
+        }
+      }
+      throw error;
+    }
   }
 
   signin() {
